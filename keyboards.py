982f6614963d8d8447e378a8924367f32caaf822
bkostenkokek <bogdankostenko83@gmail.com>
2023-05-23 23:04:31 +0300

    настройка html11

--- keyboards.py
+++ keyboards.py
@@ -1,9 +1,10 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 
-key = ReplyKeyboardMarkup(resize_keyboard=True)
-key.add('').add('Показати Дні народження').add('Видатили День народження')
-
-markup = ReplyKeyboardMarkup(resize_keyboard=True)
-markup.add(
-    KeyboardButton("Додати День народження", web_app=WebAppInfo(url="https://bkostenkokek.github.io/happy"))).add(
-    'Показати Дні народження').add('Видатили День народження')
+markup = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Видатили День народження")],
+        [KeyboardButton(text="Додати День народження WebApp",
+                        # web_app=WebAppInfo(url="https://bkostenkokek.github.io/happy"))
+                        web_app=WebAppInfo(url="https://massonnn.github.io/just-test-pages/"))
+         ]],
+    resize_keyboard=True)
